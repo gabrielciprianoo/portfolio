cecf3ef557852e962feaf736be965ccd5f324ee3
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-25 17:25:33 -0600

    ✨ feat: Add technologyIcons module to map technology names to their respective icons

---
+++ src/data/technologyIcons.ts
@@ -0,0 +1,9 @@
+import reactIcon from "../assets/icons/react.svg";
+import tailwindIcon from "../assets/icons/tailwindcss.svg";
+import typescriptIcon from "../assets/icons/typescript.svg";
+
+export const technologyIconMap: Record<string, ImageMetadata> = {
+  "React JS": reactIcon,
+  "Tailwind": tailwindIcon,
+  "Typescript": typescriptIcon,
+};
