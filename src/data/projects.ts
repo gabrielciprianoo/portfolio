c05b9a4e3e47d5d5d837e399a95a96c8dabcb1a3
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-25 17:25:30 -0600

    ✨ feat: Update Drinky project details with correct image and technology information

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,11 +11,21 @@ export type Project = {
 export const projects : Project[] = [
     {
         name : "Drinky",
-        image: "https://imageurl.com",
-        tecnologies: [],
+        image: "/images/drinky-preview.png",
+        tecnologies: ['React JS', 'Tailwind', 'Typescript'],
         resume: "Aplicacion que permite buscar recetas de bebidas basandose en ingredientes y puede generar recetas con IA",
-        highlights: [],
+        highlights: ['Estado Global con Zustand', 'Validacion con Valibot', 'OpenAI API'],
         demoUrl: 'https://drinkyapp.netlify.app/',
         repoUrl: 'https://github.com/gabrielciprianoo/drink-finder'
-    }
+    },
+    {
+        name : "Drinky",
+        image: "/images/drinky-preview.png",
+        tecnologies: ['React JS', 'Tailwind', 'Typescript'],
+        resume: "Aplicacion que permite buscar recetas de bebidas basandose en ingredientes y puede generar recetas con IA",
+        highlights: ['Estado Global con Zustand', 'Validacion con Valibot', 'OpenAI API'],
+        demoUrl: 'https://drinkyapp.netlify.app/',
+        repoUrl: 'https://github.com/gabrielciprianoo/drink-finder'
+    },
+ 
 ]
